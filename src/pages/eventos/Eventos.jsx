5fbba81cca21c143ab8189d06b3e9afe839df7fa
LucydDev <luqsy666@gmail.com>
2026-07-02 00:52:00 -0300

    v6

--- src/pages/eventos/Eventos.jsx
+++ src/pages/eventos/Eventos.jsx
@@ -5,18 +5,18 @@ import './Eventos.css';
 const fotosGaleria = [
   { id: 1, url: '../src/assets/portfolio/eventos/tutankamos-01.jpg' },
   { id: 2, url: '../src/assets/portfolio/eventos/tutankamos-03.jpg' },
-  { id: 3, url: '../src/assets/portfolio/eventos/tutankamos-04.jpg' }
-  { id: 4, url: '../src/assets/portfolio/eventos/tutankamos-05.jpg' }
-  { id: 5, url: '../src/assets/portfolio/eventos/tutankamos-06.jpg' }
-  { id: 6, url: '../src/assets/portfolio/eventos/tutankamos-07.jpg' }
-  { id: 7, url: '../src/assets/portfolio/eventos/tutankamos-08.jpg' }
-  { id: 8, url: '../src/assets/portfolio/eventos/tutankamos-09.jpg' }
-  { id: 9, url: '../src/assets/portfolio/eventos/tutankamos-10.jpg' }
-  { id: 10, url: '../src/assets/portfolio/eventos/tutankamos-11.jpg' }
-  { id: 11, url: '../src/assets/portfolio/eventos/tutankamos-12.jpg' }
-  { id: 12, url: '../src/assets/portfolio/eventos/arfw-01.jpg' }
-  { id: 13, url: '../src/assets/portfolio/eventos/arfw-02.jpg' }
-  { id: 14, url: '../src/assets/portfolio/eventos/arfw-03.jpg' }
+  { id: 3, url: '../src/assets/portfolio/eventos/tutankamos-04.jpg' },
+  { id: 4, url: '../src/assets/portfolio/eventos/tutankamos-05.jpg' },
+  { id: 5, url: '../src/assets/portfolio/eventos/tutankamos-06.jpg' },
+  { id: 6, url: '../src/assets/portfolio/eventos/tutankamos-07.jpg' },
+  { id: 7, url: '../src/assets/portfolio/eventos/tutankamos-08.jpg' },
+  { id: 8, url: '../src/assets/portfolio/eventos/tutankamos-09.jpg' },
+  { id: 9, url: '../src/assets/portfolio/eventos/tutankamos-10.jpg' },
+  { id: 10, url: '../src/assets/portfolio/eventos/tutankamos-11.jpg' },
+  { id: 11, url: '../src/assets/portfolio/eventos/tutankamos-12.jpg' },
+  { id: 12, url: '../src/assets/portfolio/eventos/arfw-01.jpg' },
+  { id: 13, url: '../src/assets/portfolio/eventos/arfw-02.jpg' },
+  { id: 14, url: '../src/assets/portfolio/eventos/arfw-03.jpg' },
   { id: 15, url: '../src/assets/portfolio/eventos/arfw-04.jpg' }
 ];
 
